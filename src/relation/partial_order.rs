54dc4ec4dff0c0aba7e2e200c0a1033c7b9a3491
Wilke Schwiedop <wilke.schwiedop.2010w@informatik.h-brs.de>
2019-04-24 14:22:19 +0200

    implement PO operations
    - upset and downset
    - upper and lower bounds
    - greatest and smallest elements
    - infimum and supremum

--- src/relation/partial_order.rs
+++ src/relation/partial_order.rs
@@ -1,8 +1,74 @@
+use crate::set::{Set, SetElement};
 use crate::relation::Relation;
 use crate::relation::Endorelation;
 use crate::relation::relation::{Complement, Concatenation, Converse, Intersection, Union};
 
 pub trait PartialOrder : Endorelation {
+	/// Return the (principal) upset of the relation.
+	/// Given a partial order `R` over the set `U` and an element `x ∈ U`,
+	/// the upset is the set `{ y ∈ U | xRy }`
+	fn upset(&self, x: &SetElement) -> Set {
+		debug_assert!(self.is_partial_order());
+		debug_assert!(self.get_domain().0.contains(x));
+		let ix = self.get_domain().0.iter().position(|e| e == x).unwrap();
+		self.get_domain().0.iter().enumerate()
+			.filter(|&(iy, _)| self.eval_at(ix, iy))
+			.map(|(_, y)| y)
+			.cloned()
+			.collect()
+	}
+	/// Return the (principal) downset of the relation.
+	/// Given a partial order `R` over the set `U` and an element `x ∈ U`,
+	/// the upset is the set `{ y ∈ U | yRx }`
+	fn downset(&self, x: &SetElement) -> Set {
+		debug_assert!(self.is_partial_order());
+		debug_assert!(self.get_domain().0.contains(x));
+		PartialOrder::upset(&Self::converse(self), x)
+	}
+	/// upr_R(u) := { y ∈ U | ∀x ∈ u: xRy }
+	fn bound_upper(&self, u: &Set) -> Set {
+		debug_assert!(self.is_partial_order());
+		debug_assert!(u.is_subset(self.get_domain().0));
+		let ixs: Vec<usize> = Set::intersection_enumerated(self.get_domain().0, u)
+			.map(|((ix, _), _)| ix)
+			.collect();
+		self.get_domain().0.iter().enumerate()
+			.filter(|&(iy, _)| ixs.iter().all(|&ix| self.eval_at(ix, iy)))
+			.map(|(_, y)| y)
+			.cloned()
+			.collect()
+	}
+	/// lwr_R(u) := { y ∈ U | ∀x ∈ u: yRx }
+	fn bound_lower(&self, u: &Set) -> Set {
+		debug_assert!(self.is_partial_order());
+		debug_assert!(u.is_subset(self.get_domain().0));
+		PartialOrder::bound_upper(&Self::converse(self), u)
+	}
+	/// grt_R(u) := upr_R(u) ∩ u
+	fn elements_greatest(&self, u: &Set) -> Set {
+		debug_assert!(self.is_partial_order());
+		debug_assert!(u.is_subset(self.get_domain().0));
+		Set::intersection(&self.bound_upper(u), u).cloned().collect()
+	}
+	/// sml_R(u) := lwr_R(u) ∩ u
+	fn elements_smallest(&self, u: &Set) -> Set {
+		debug_assert!(self.is_partial_order());
+		debug_assert!(u.is_subset(self.get_domain().0));
+		Set::intersection(&self.bound_lower(u), u).cloned().collect()
+	}
+	/// sup_R(u) := sml_R(upr_R(u))
+	fn supremum(&self, u: &Set) -> Set {
+		debug_assert!(self.is_partial_order());
+		debug_assert!(u.is_subset(self.get_domain().0));
+		self.elements_smallest(&self.bound_upper(u))
+	}
+	/// inf_R(u) := grt_R(lwr_R(u))
+	fn infimum(&self, u: &Set) -> Set {
+		debug_assert!(self.is_partial_order());
+		debug_assert!(u.is_subset(self.get_domain().0));
+		self.elements_greatest(&self.bound_lower(u))
+	}
+
 	/// Return `true` if the relation is a lattice.
 	fn is_lattice(&self) -> bool {
 		debug_assert!(self.is_partial_order());
